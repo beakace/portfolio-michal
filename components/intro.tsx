92bd65dfe2d2e0db9faab71c455886efd4a9108c
beakace <michaldziuba26@gmail.com>
2023-08-26 01:08:48 +0200

    Add first couple sections

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,7 +9,10 @@ import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
-    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+    <section
+      id="home"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -41,7 +44,7 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
-      <motion.p
+      <motion.h1
         className="mb-10 mt-4 px-4 font-medium !leading-[1.5] text-2xl sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
@@ -52,7 +55,7 @@ export default function Intro() {
         <span className="font-bold">28 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
-      </motion.p>
+      </motion.h1>
       <motion.div
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
         initial={{ opacity: 0, y: 100 }}
@@ -74,10 +77,18 @@ export default function Intro() {
           Download CV
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
-        <a className="bg-white text-gray-700 px-4 py-3 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:text-gray-950 hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10" href="https://linkedin.com" target='_blank'>
+        <a
+          className="bg-white text-gray-700 px-4 py-3 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:text-gray-950 hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://linkedin.com"
+          target="_blank"
+        >
           <BsLinkedin />
         </a>
-        <a className="bg-white text-gray-700 text-[1.35rem] px-4 py-3 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:text-gray-950 hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10" href="https://github.com/beakace" target='_blank'>
+        <a
+          className="bg-white text-gray-700 text-[1.35rem] px-4 py-3 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:text-gray-950 hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://github.com/beakace"
+          target="_blank"
+        >
           <FaGithubSquare />
         </a>
       </motion.div>
---
+++ components/projects.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { projectsData } from "@/lib/data";
+
+import SectionHeading from "./section-heading";
+import Project from "./project";
+
+export default function Projects() {
+  return (
+    <section id="projects" className="scroll-mt-28">
+      <SectionHeading>My projects</SectionHeading>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}
