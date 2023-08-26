036e2fe83fc7a05cac4d5dc4e37fee166861b01a
beakace <michaldziuba26@gmail.com>
2023-08-26 15:27:07 +0200

    Fix font

--- components/contact.tsx
+++ components/contact.tsx
@@ -51,7 +51,7 @@ export default function Contact() {
         }}
       >
         <input
-          className="h-14 px-4 rounded-lg borderBlack transition-all dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 dark:outline-none"
+          className="h-14 px-4 rounded-lg borderBlack transition-all dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 dark:text-black/70 dark:outline-none"
           name="senderEmail"
           type="email"
           required
@@ -59,7 +59,7 @@ export default function Contact() {
           placeholder="Your email"
         />
         <textarea
-          className="h-52 my-3 rounded-lg borderBlack p-4 transition-all dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 dark:outline-none"
+          className="h-52 my-3 rounded-lg borderBlack p-4 transition-all dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 dark:text-black/70 dark:outline-none"
           name="message"
           placeholder="Your message"
           required
