b7cb7af65c7e7b67c6327edeccd2c9324c167a13
beakace <michaldziuba26@gmail.com>
2023-08-26 14:47:13 +0200

    Fix some bugs

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,9 +1,7 @@
 "use client";
+
 import { useTheme } from "@/context/theme-context";
 import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
-
 import { BsMoon, BsSun } from "react-icons/bs";
 
 export default function ThemeSwitch() {
@@ -11,7 +9,7 @@ export default function ThemeSwitch() {
 
   return (
     <button
-      className=" bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all border-white border-opacity-40 shadow-2xl fixed bottom-5 right-5 dark:bg-gray-950"
+      className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
       onClick={toggleTheme}
     >
       {theme === "light" ? <BsSun /> : <BsMoon />}
