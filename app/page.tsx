ad601cfc1548314d8548117aa0faf1947e9101af
beakace <michaldziuba26@gmail.com>
2023-08-31 23:34:10 +0200

    Change font and rearange sections

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,10 @@ export default function Home() {
     <main className="flex flex-col items-center px-4">
       <Intro />
       <SectionDivider />
-      <About />
-      <Projects />
       <Skills />
+      <Projects />
       <Experience />
+      <About />
       <Contact />
     </main>
   );
