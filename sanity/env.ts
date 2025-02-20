fdb76e2c769b3d791cfe75cf7fca5abc6973ffab
beakace <michaldziuba26@gmail.com>
2025-02-20 23:04:39 +0100

    Fix

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,12 +1,7 @@
 export const apiVersion = "2024-03-19";
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
-);
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
-);
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
+export const projectId =
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "a6yeub3t";
 export const useCdn = false;
 
 // Export for easier imports
@@ -16,13 +11,5 @@ export const config = {
   apiVersion,
   useCdn,
 };
-
-function assertValue<T>(v: T | undefined, errorMessage: string): T {
-  if (v === undefined) {
-    throw new Error(errorMessage);
-  }
-
-  return v;
-}
 
 console.log("Sanity Client Config:", { projectId, dataset, apiVersion });
